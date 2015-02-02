980a283695de4b5bfa9e592e7c7e237f69048475
PTank <akazian@student.42.fr>
2015-02-02 01:01:31 +0100

    merge end

--- install.sh
+++ install.sh
@@ -59,6 +59,8 @@ then
 			ln -s $DIR$name ~/$name
 		fi
 	done
+	mkdir -p .vim
+	mkdir -p .vim/bundle
 	if [ ! -e ~/.vim/bundle/Vundle.vim ]
 	then
 		git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
